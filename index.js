e8463dbf5e3b5d4514c4106c1e6261e15a3e4f6c
lokintro <danielakarinaceballes@gmail.com>
2023-04-15 08:45:51 -0300

    prueba con append

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-const suma = document.getElementsByClassName("mas")
-const menos = document.getElementsByClassName("menos")
-
-var numb = 0;
-
-function sumando() {
-
-    numb++
-    set_numb(numb)
-}
+let body = document.getElementsByTagName("body")[0];
 
 
-function restar() {
-    numb--
-    set_numb(numb)
-}
-function set_numb(value) {
-    if (numb >= 0) {
-        
+
+
+for (let i = 0; i < 10; i++) {
+    let element = document.createElement("div");
+    let elemento2 = document.createElement("div");
+    let elemento3 = document.createElement("div");
+    elemento2.classList += "secondLine"
+    elemento3.classList += "thirdLine"
+    body.appendChild(element)
+    body.appendChild(elemento2)
+    body.appendChild(elemento3)
     
-    document.querySelectorAll("h1").forEach(element => {
-        element.innerHTML = value
-    });
 }
-}
+/*
+let elemento2 = document.createElement("img")
+elemento2.setAttribute("src","https://plus.unsplash.com/premium_photo-1661315433213-a97f4706b566?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80") 
 
-
-
-
+element.appendChild(elemento2);*/
